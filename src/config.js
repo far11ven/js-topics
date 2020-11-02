6141227bb4365cf18fc5939b50976e374ee2783e
kushal bhalaik <kushal@omnius.com>
2020-11-02 21:29:11 +0530

    updated mongodb-realm endpoints

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
   API: {
     GET_TOPIC:
-      "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/torrent_trak-callp/service/JSTopicsServices/incoming_webhook/get_topic?secret=SECRET",
+      "https://webhooks.mongodb-realm.com/api/client/v2.0/app/torrent_trak-callp/service/JSTopicsServices/incoming_webhook/get_topic?secret=SECRET",
     SAVE_TOPIC:
-      "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/torrent_trak-callp/service/JSTopicsServices/incoming_webhook/save_topic?secret=SECRET"
+      "https://webhooks.mongodb-realm.com/api/client/v2.0/app/torrent_trak-callp/service/JSTopicsServices/incoming_webhook/save_topic?secret=SECRET"
   }
 };
